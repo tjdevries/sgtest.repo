37883c80a195d63ae374098b4325b4bdb4bc60d7
TJ DeVries <devries.timothyj@gmail.com>
2022-03-02 10:38:57 -0500

    make it valid maybe

--- multiline.cs
+++ multiline.cs
@@ -1,3 +1,14 @@
-var x = @"Multi
-Line
-String";
+namespace Radzen.Blazor.Tests
+{
+    public class DataGridTests
+    {
+        // Css classes tests
+        [Fact]
+        public void DataGrid_Renders_CssClass()
+        {
+            var x = @$"Multi
+            Line
+            String";
+        }
+    }
+}
